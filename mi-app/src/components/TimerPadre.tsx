ea31a60f9d595b072d8371033850726b2fefeb18
Luciano <luciano_flores42@hotmail.com>
2024-08-16 11:07:42 -0300

    testeo2

--- mi-app/src/components/TimerPadre.tsx
+++ mi-app/src/components/TimerPadre.tsx
@@ -8,9 +8,10 @@ export const TimerPadre = () => {
       <h2>useEffect</h2>
         <span>Milisegundos: {milisegundos}</span>
         <button className="btn btn-primary" onClick={()=> setMilisegundos(1000)}>1000</button>
-        <button className="btn btn-success" onClick={()=> setMilisegundos(2000)}>2000</button>
+        <button className="btn btn-success" onClick={()=> setMilisegundos(500)}>500</button>
+        <button className="btn btn-success" onClick={()=> setMilisegundos(250)}>250</button>
 
-        <Timer/>
+        <Timer milisegundos={milisegundos}/>
     </div>
   )
 }
